16776346e6ff2bdfbc78c97db405dcfa8de394b3
corvert <orvet.priimagi@gmail.com>
2026-03-10 15:12:27 +0200

    Add CsrfController to expose CSRF token and update SecurityConfig for CSRF protection

--- tracker/src/main/java/investTracker/security/SecurityConfig.java
+++ tracker/src/main/java/investTracker/security/SecurityConfig.java
@@ -10,12 +10,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import java.time.LocalDate;
 
@@ -28,14 +27,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
         http.authorizeHttpRequests((requests) ->
                 requests
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/api/csrf-token").permitAll()
                         .anyRequest().authenticated());
         //http.formLogin(withDefaults());
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.addFilterBefore(new CustomLoggingFilter(), UsernamePasswordAuthenticationFilter.class);
         http.httpBasic(withDefaults());
         return http.build();
     }
